2d4bd53138d0dd659ac79f99d006f82882d01839
minkhant285 <bryyan.mk@gmail.com>
2022-04-23 16:30:19 +0630

    added build folder

--- deploy.js
+++ deploy.js
@@ -10,4 +10,4 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => console.log(`App is live on port ${port}!`))
+app.listen(port, () => console.log(`App is live on port : ${port}!`))
